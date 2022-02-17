b288fd67eafbcfe7f22aefa4f116a3ab85bd8a09
하은지 <haeunji5072@gmail.com>
2022-02-17 21:54:59 +0900

    💄 RepositoryListCell UI 속성 추가 및 AutoLayout 설정 (by SnapKit)

--- GithubRepository/GithubRepository/RepositoryListCell.swift
+++ GithubRepository/GithubRepository/RepositoryListCell.swift
@@ -9,7 +9,8 @@ import UIKit
 import SnapKit
 
 class RepositoryListCell: UITableViewCell {
-    var repository: String?
+    
+    var repository: Repository?
     
     // MARK: Cell의 UI 구성
     let nameLabel = UILabel()
@@ -27,7 +28,54 @@ class RepositoryListCell: UITableViewCell {
         ].forEach {
             contentView.addSubview($0)
         }
+        
+        // MARK: UI의 속성
+        guard let repository = repository else {
+            return
+        }
+        
+        nameLabel.text = repository.name
+        nameLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        
+        descriptionLabel.text = repository.description
+        descriptionLabel.font = .systemFont(ofSize: 15)
+        descriptionLabel.numberOfLines = 2
+        
+        starImageView.image = UIImage(systemName: "star")
+        
+        starLabel.text = "\(repository.stargazersCount)"
+        starLabel.font = .systemFont(ofSize: 16)
+        starLabel.textColor = .gray
+        
+        languageLabel.text = repository.language
+        languageLabel.font = .systemFont(ofSize: 16)
+        languageLabel.textColor = .gray
+        
+        // MARK: AutoLayout을 위한 Snapkit 설정
+        nameLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(18)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.bottom).offset(3)
+            $0.leading.trailing.equalTo(nameLabel)
+        }
+        
+        starImageView.snp.makeConstraints{
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(1) // 8
+            $0.leading.equalTo(descriptionLabel)
+            $0.width.height.equalTo(20)
+            $0.bottom.equalToSuperview().inset(18)
+        }
+        
+        starLabel.snp.makeConstraints{
+            $0.centerY.equalTo(starImageView)
+            $0.leading.equalTo(starImageView.snp.trailing).inset(5)
+        }
+        
+        languageLabel.snp.makeConstraints{
+            $0.centerY.equalTo(starLabel)
+            $0.leading.equalTo(starLabel.snp.trailing).inset(12)
+        }
     }
 }
-
-
