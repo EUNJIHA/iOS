e655eaafa8ea7e858dcda59f1f0a7a568106a28d
하은지 <haeunji5072@gmail.com>
2022-02-17 21:10:45 +0900

    💄 RepositoryListCell UI 구성, RepositoryListViewController UI 구성 및 refreshControl 추가

--- GithubRepository/GithubRepository/RepositoryListCell.swift
+++ GithubRepository/GithubRepository/RepositoryListCell.swift
@@ -6,5 +6,28 @@
 //
 
 import UIKit
+import SnapKit
+
+class RepositoryListCell: UITableViewCell {
+    var repository: String?
+    
+    // MARK: Cell의 UI 구성
+    let nameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let starImageView = UIImageView()
+    let starLabel = UILabel()
+    let languageLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        [
+            nameLabel, descriptionLabel,
+            starImageView, starLabel, languageLabel
+        ].forEach {
+            contentView.addSubview($0)
+        }
+    }
+}
 
 
--- GithubRepository/GithubRepository/RepositoryListViewController.swift
+++ GithubRepository/GithubRepository/RepositoryListViewController.swift
@@ -8,5 +8,42 @@
 import UIKit
 
 class RepositoryListViewController: UITableViewController {
+    private let origanization = "Apple"
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // MARK: Title
+        title = origanization + "Repositories"
+        
+        // MARK: API Call
+        refreshControl = UIRefreshControl()
+        let refreshControl = refreshControl!
+        refreshControl.backgroundColor = .white
+        refreshControl.tintColor = .darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        // MARK: register Cell
+        tableView.register(RepositoryListCell.self, forCellReuseIdentifier: "RepositoryListCell")
+        tableView.rowHeight = 140
+    }
+    
+    // MARK: API Call
+    @objc func refresh() {
+        
+    }
+}
+
+// MARK: UITableView DataSource Delegate
+extension RepositoryListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryListCell", for: indexPath) as? RepositoryListCell else {return UITableViewCell()}
+        
+        return cell
+    }
 }
