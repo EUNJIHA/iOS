a58ac3918ee65469f490fd021622eea21525237b
하은지 <haeunji5072@gmail.com>
2022-05-19 15:11:16 +0900

    ✨ Create SearchController

---
+++ Practice/Practice/README.md
@@ -0,0 +1,4 @@
+#  Practice
+
+![SearchController](https://user-images.githubusercontent.com/43839938/169222569-ed4ffed7-8cb7-4936-8575-6f8e917a7791.png)
+
--- Practice/Practice/ViewController.swift
+++ Practice/Practice/ViewController.swift
@@ -4,16 +4,43 @@
 //
 //  Created by mac on 2022/05/19.
 //
+// https://developer.apple.com/documentation/uikit/view_controllers/using_suggested_searches_with_a_search_controller
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    fileprivate func createSearchController() {
+        
+        let searchController = UISearchController(searchResultsController: nil)
+//        searchController.searchResultsUpdater = self
+        searchController.searchBar.autocapitalizationType = .none
+        searchController.searchBar.searchTextField.placeholder = NSLocalizedString("Enter a search term", comment: "")
+        searchController.searchBar.returnKeyType = .done
+        
+        // Place the search bar in the navigation bar.
+        navigationItem.searchController = searchController
+        
+        // Make the search bar always visible.
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        // Monitor when the search controller is presented and dismissed.
+        searchController.delegate = self
+        
+        // Monitor when the search button is tapped, and start/end editing.
+        searchController.searchBar.delegate = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        createSearchController()
     }
 
 
 }
 
+extension ViewController: UISearchControllerDelegate, UISearchBarDelegate {
+    
+}
